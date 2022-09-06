bd0b1f8f40d4480cb8549325ee4c6a9f9ea5af14
RicardoPineda2301 <rp230198@gmail.com>
2022-09-06 17:52:24 -0600

    Ejercicio 02

--- Ejercicio_02/src/Orden.java
+++ Ejercicio_02/src/Orden.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Orden {
+import java.util.Date;
+import java.lang.Number;
+
+public class Orden {
+    Number orden;
+    Number ContadorProductos;
+
+    public Orden(Number orden, Number contadorProductos) {
+        this.orden = orden;
+        ContadorProductos = contadorProductos;
+    }
+
+    public Number getOrden() {
+        return orden;
+    }
+
+    public void setOrden(Number orden) {
+        this.orden = orden;
+    }
+
+    public Number getContadorProductos() {
+        return ContadorProductos;
+    }
+
+    public void setContadorProductos(Number contadorProductos) {
+        ContadorProductos = contadorProductos;
+    }
 }
--- Ejercicio_02/src/Producto.java
+++ Ejercicio_02/src/Producto.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Producto {
+import java.util.Date;
+import java.lang.Number;
+
+public class Producto {
+    Number IdProducto;
+    String Nombre;
+    Number precio;
+    Number ContadorProducto;
+
+    public Producto(Number idProducto, String nombre, Number precio, Number contadorProducto) {
+        IdProducto = idProducto;
+        Nombre = nombre;
+        this.precio = precio;
+        ContadorProducto = contadorProducto;
+    }
+
+    public Number getIdProducto() {
+        return IdProducto;
+    }
+
+    public void setIdProducto(Number idProducto) {
+        IdProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public Number getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Number precio) {
+        this.precio = precio;
+    }
+
+    public Number getContadorProducto() {
+        return ContadorProducto;
+    }
+
+    public void setContadorProducto(Number contadorProducto) {
+        ContadorProducto = contadorProducto;
+    }
 }

83a8788310e51c56e0db133063d3828aa549696b
RicardoPineda2301 <rp230198@gmail.com>
2022-09-13 20:59:45 -0600

    MVN clase 01

---
+++ ClaseMaven01/src/test/java/DemoTestNG.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class DemoTestNG {
+
+    @Test
+    public void firstTestExample(){
+        System.out.println("This is my first test NG");
+    }
+
+    @Test
+    public void secondTestExample(){
+        System.out.println("This is my second test NG");
+    }
+
+
+}
